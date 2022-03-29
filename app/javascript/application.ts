c32737704d22887d403efb7b788b5597edc97705
Tamara <tylues@uc.cl>
2022-03-28 23:13:36 -0300

    feat(): change application js to typescript configuration

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
-import App from './components/app.vue';
 import './css/application.css';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp({
-    components: { App },
+    components: {
+    },
   });
+
   app.mount('#vue-app');
 
   return app;

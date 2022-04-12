a145105e384132eeb34b5d5172b0563e47a9b3f8
Tamara <tylues@uc.cl>
2022-04-11 23:04:17 -0400

    feat(navbar): add navbar component with responsive behaviour

---
+++ app/javascript/components/shared/nav-bar.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+  <nav
+    class="absolute top-0 left-0 right-0 z-10 items-center py-5"
+  >
+    <img
+      :src="require('../../../assets/images/bichito-logo.svg')"
+      class="w-24 mx-10 fill-blue-300 lg:w-32 md:mx-32 lg:mx-56"
+      alt="Logo Bichito"
+    >
+  </nav>
+</template>

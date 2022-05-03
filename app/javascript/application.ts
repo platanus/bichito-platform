50f03a5c2c7bb11e59159deca6a7cbd98f755817
Tamara <tylues@uc.cl>
2022-05-03 17:35:18 -0400

    feat(application): add button, hero section and navbar to application file

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,13 +1,19 @@
 import { createApp } from 'vue';
 import './css/application.css';
 
+import NavBar from './components/shared/nav-bar.vue';
+import BichitoButton from './components/shared/bichito-button.vue';
+import LandingHero from './components/landing/landing-hero.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp({
     components: {
+      NavBar,
+      LandingHero,
     },
   });
 
+  app.component('BichitoButton', BichitoButton);
   app.mount('#vue-app');
 
   return app;

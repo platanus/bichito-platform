abf3664b55376890bf29affa32a4f69e51aa14ad
Tamara <tylues@uc.cl>
2022-05-03 17:34:58 -0400

    feat(button): implement reusable button component of design system

---
+++ app/javascript/components/shared/bichito-button.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+  <component
+    :is="$attrs.href ? 'a' : 'button'"
+    class="p-4 text-sm font-bold text-white rounded-full bg-slate-800 hover:bg-slate-700 hover:shadow-lg sm:px-6 sm:text-base"
+    v-bind="$attrs"
+  >
+    <slot />
+  </component>
+</template>

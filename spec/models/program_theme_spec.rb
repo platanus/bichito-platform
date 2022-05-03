b2125cf63c28da9f128e55256a9746b3f13736ad
Tamara <tylues@uc.cl>
2022-05-03 16:57:31 -0400

    test(program_theme): add program them model test

--- spec/models/program_theme_spec.rb
+++ spec/models/program_theme_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ProgramTheme, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with valid attributes' do
+    it { expect(build(:program_theme)).to be_valid }
+  end
 end

ad92a12bc013bb2a3725c0e081783056f0cb48ee
MariaJosefinaAlliende <moalliende@uc.cl>
2022-05-10 17:49:01 -0400

    feat(landing): display navbar, hero and challenges

--- app/javascript/components/landing/landing-hero.vue
+++ app/javascript/components/landing/landing-hero.vue
@@ -3,7 +3,7 @@ import BichitoButton from '../shared/bichito-button.vue';
 </script>
 
 <template>
-  <section class="container flex flex-col items-center px-4 mx-auto mt-28">
+  <section class="container flex flex-col items-center px-4 mx-auto mt-28 mb-10">
     <div class="flex flex-col items-center sm:w-8/12 xl:w-7/12">
       <h1 class="mb-48 text-2xl font-semibold text-center text-slate-800 sm:mb-12 lg:text-4xl">
         Curso de programación para estudiantes de enseñanza media
@@ -11,11 +11,11 @@ import BichitoButton from '../shared/bichito-button.vue';
     </div>
     <img
       :src="require('../../../assets/images/bee-path.svg')"
-      class="absolute left-0 w-full px-4 top-48 sm:top-36 md:w-11/12 lg:top-24 lg:left-10 lg:w-10/12"
+      class="absolute top-48 left-0 px-4 w-full sm:top-36 md:w-11/12 lg:top-24 lg:left-10 lg:w-10/12"
     >
     <img
       :src="require('../../../assets/images/code-picture.png')"
-      class="absolute w-9/12 sm:w-7/12 lg:w-6/12 top-52"
+      class="absolute top-52 w-9/12 sm:w-7/12 lg:w-6/12"
     >
     <div class="sm:mt-48 xl:mt-80 2xl:mt-96">
       <bichito-button

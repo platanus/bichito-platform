712de57bda7bb3abb708f75802dbb2cd5e7e1f94
MariaJosefinaAlliende <moalliende@uc.cl>
2022-05-11 17:52:41 -0400

    feat(challenges/LandingChallenges): use challenges from back

--- app/javascript/components/landing/landing-challenges.vue
+++ app/javascript/components/landing/landing-challenges.vue
@@ -1,52 +1,49 @@
 <script setup lang='ts'>
+import { ref, onMounted } from 'vue';
 import ChallengeCard from './challenge-card.vue';
+import challengeApi from '../../api/challenge';
 
-const challenges = [
-  { id: 1,
-    title: 'Pizzapp',
-    stack: 'python',
-    level: 'easy',
-    description:
-      `Programarás una aplicación para pedir pizza y calcular cuánto debe pagar cada persona.
-      ¡Usando operaciones matemáticas y prints te asegurarás de que todos paguen!`,
-  },
-  { id: 2,
-    title: 'Chatbot',
-    stack: 'python',
-    level: 'medium',
-    description:
-      `Crearás un chatbot que salude, cuente chistes y ser ría.
-      ¡Aplicando tus conocimientos de inputs y prints tu chatbot será chistoso!`,
-  },
-  { id: 3,
-    title: 'PrograEats',
-    stack: 'python',
-    level: 'hard',
-    description:
-      `Programarás el menú de una app de comida rápida.
-      ¡Usando condicionales if, elif y else podrás pedir tu comida favorita!`,
-  },
-];
+const challenges = ref(null);
+const loading = ref(true);
+const error = ref(false);
+const CHALLENGES_LIMIT = 3;
+
+async function getChallenges() {
+  try {
+    const response = await challengeApi.get();
+    challenges.value = response.data.challenges.slice(0, CHALLENGES_LIMIT);
+    loading.value = false;
+  } catch (e) {
+    error.value = true;
+  }
+}
+
+onMounted(() => {
+  getChallenges();
+});
 
 </script>
 
 <template>
-  <section class="flex flex-col items-center px-8 pt-10 w-full bg-slate-900 lg: lg:px-56 lg:pt-16">
-    <div class="flex flex-col items-center w-full lg:items-start ">
-      <h1 class="text-xl font-semibold text-center text-white lg:text-3xl lg:text-left ">
+  <section class="flex flex-col items-center px-8 pt-10 w-full bg-slate-900 md:pt-16 lg:px-56">
+    <div class="flex flex-col items-center w-full md:items-start ">
+      <h1 class="text-xl font-semibold text-center text-white md:text-3xl md:text-left ">
         ¿Quieres familiarizarte?
       </h1>
-      <div class="pt-8 text-sm text-center text-white lg:pt-5 lg:text-lg lg:text-left">
+      <div class="pt-8 text-sm text-center text-white md:pt-5 md:text-lg md:text-left">
         Tenemos una recopilación de guías y desafíos que puedes intentar por tu cuenta por mientras
       </div>
     </div>
-    <div class="flex overflow-x-auto flex-row pt-11 w-full lg:grid lg:grid-cols-3 lg:gap-8">
+    <div
+      v-if="!loading"
+      class="flex overflow-x-auto flex-row pt-11 w-full md:grid md:grid-cols-3 md:gap-8"
+    >
       <challenge-card
         v-for="challenge in challenges"
         :key="`challenge-cad-${challenge.id}`"
         :title="challenge.title"
         :stack="challenge.stack"
-        :level="challenge.level"
+        :difficulty="challenge.difficulty"
         :description="challenge.description"
       />
     </div>

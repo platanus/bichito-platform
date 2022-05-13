e8b22831efaa427b46fc893ea62317ba8b60c7a0
Tamara <tylues@uc.cl>
2022-05-13 18:52:07 -0400

    test(): add basic member model test

---
+++ spec/factories/members.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :member do
+    name { "MyString" }
+    description { "MyText" }
+    image_data { "" }
+    role { "MyString" }
+  end
+end
---
+++ spec/models/member_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Member, type: :model do
+  it 'has a valid factory' do
+    expect(build(:member)).to be_valid
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :role }
+    it { is_expected.to validate_presence_of :image }
+    it { is_expected.to validate_presence_of :description }
+  end
+end

aaccad9413a3339e4de278da9054f6dd3bd0cd9c
Tamara <tylues@uc.cl>
2022-05-13 18:46:03 -0400

    feat(application): add new components to application.ts file

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -7,6 +7,8 @@ import LandingHero from './components/landing/landing-hero.vue';
 import LandingChallenges from './components/landing/landing-challenges.vue';
 import LandingPosts from './components/landing/landing-posts.vue';
 import LandingInvitation from './components/landing/landing-invitation.vue';
+import MemberCard from './components/landing/member-card.vue';
+import MembersSection from './components/landing/members-section.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp({
@@ -16,10 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
       LandingChallenges,
       LandingPosts,
       LandingInvitation,
+      MembersSection,
     },
   });
 
   app.component('BichitoButton', BichitoButton);
+  app.component('MemberItem', MemberCard);
   app.mount('#vue-app');
 
   return app;

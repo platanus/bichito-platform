94d1a1a00a33acceacedc1a8d9bae23b57881367
MariaJosefinaAlliende <moalliende@uc.cl>
2022-05-13 17:02:37 -0400

    feat(post-images/PostCard): display image

--- app/javascript/components/landing/post-card.vue
+++ app/javascript/components/landing/post-card.vue
@@ -1,14 +1,14 @@
 <script setup lang='ts'>
 interface Props {
   title: string,
-  image?: string,
+  imageUrl?: string,
   variant: string,
   bgCards: boolean,
 }
 
 withDefaults(defineProps<Props>(), {
   title: 'Actividad Bichito',
-  image: undefined,
+  imageUrl: undefined,
   variant: 'center',
   bgCards: false,
 });
@@ -28,10 +28,15 @@ const variantStyles = {
       <div class="aspect-square absolute z-0 w-full bg-slate-100 rounded-3xl shadow-xl -rotate-12" />
     </div>
     <div
-      class="flex relative flex-col items-center p-4 md:p-5 w-full h-max bg-white rounded-xl md:rounded-2xl lg:rounded-3xl border border-slate-100 shadow-2xl"
+      class="flex relative flex-col items-center p-4 md:p-5 w-full h-max bg-white rounded-xl lg:rounded-2xl border border-slate-100 shadow-2xl"
       :class="variantStyles[variant]"
     >
-      <div class="aspect-21/20 flex flex-col p-4 mb-4 md:mb-6 w-full bg-slate-200 rounded-xl md:rounded-2xl lg:rounded-3xl " />
+     <img
+        v-if="imageUrl"
+        :src="imageUrl"
+        class="object-cover aspect-21/20 flex flex-col mb-4 md:mb-6 w-full rounded-2xl "
+      > 
+      <div v-else class="aspect-21/20 flex flex-col p-4 mb-4 md:mb-6 w-full bg-slate-200 rounded-xl md:rounded-2xl " />
       <div class="w-full text-left text-slate-700 md:text-base lg:text-lg">
         {{ title }}
       </div>

b0004c3cb3824e59f03bd40295829627bb8abdbe
MariaJosefinaAlliende <moalliende@uc.cl>
2022-05-13 14:29:11 -0400

    feat(posts/LandingPosts): show posts from back

--- app/javascript/components/landing/landing-posts.vue
+++ app/javascript/components/landing/landing-posts.vue
@@ -1,25 +1,47 @@
 <script setup lang='ts'>
+import { ref, computed, onMounted } from 'vue';
 import PostCard from './post-card.vue';
+import postApi from '../../api/post';
 
-const posts = [
-  { id: 1, variant: 'left', title: 'Bienvenida 2019' },
-  { id: 2, variant: 'center', title: 'Visita a colegio 2021', bgCards: true },
-  { id: 3, variant: 'right', title: 'Visita a colegio 2021' },
-];
+
+const POSTS_LIMIT = 3;
+const POSTS_VARIANTS = ['left', 'center', 'right'];
+
+const posts = ref(null);
+const loading = ref(true);
+const error = ref(false);
+
+async function getPosts() {
+  try {
+    const response = await postApi.get();
+    posts.value = response.data.posts.slice(0, POSTS_LIMIT);
+    loading.value = false;
+  } catch (e) {
+    error.value = true;
+  }
+}
+
+onMounted(() => {
+  getPosts();
+});
+
 
 </script>
 
 <template>
-  <section class="flex relative flex-col items-center px-4 pb-16 md:pb-14">
+  <section
+    v-if="!loading"
+    class="flex relative flex-col items-center px-4 pb-16 md:pb-14"
+  >
     <img
       :src="require('../../../assets/images/bee-path-2.svg')"
       class="z-30 h-2/3 rotate-12 md:rotate-0 absolute -top-12 md:top-0 -left-16 md:-left-4 lg:left-0 md:h-auto"
     >
     <div class="mt-28 w-[80%] md:grid md:grid-cols-3">
       <post-card
-        v-for="post in posts "
+        v-for="(post, id) in posts "
         :key="`news-card-${post.id}`"
-        :variant="post.variant"
+        :variant="POSTS_VARIANTS[id]"
         :title="post.title"
         :bg-cards="post.bgCards"
       />

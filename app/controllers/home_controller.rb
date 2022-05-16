ae4abcf5ad148bf0ed28ea72e12fe6fbb6711ce7
MariaJosefinaAlliende <moalliende@uc.cl>
2022-05-16 16:25:25 -0400

    feat(hide-challenges/HomeController): fetch env variable to show challenges

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
+  SHOW_CHALLENGES = ENV.fetch('SHOW_CHALLENGES') == 'true'
   def index
     @program_themes = ProgramTheme.all
     @members = Member.all
+    @show_challenges = SHOW_CHALLENGES
   end
 end

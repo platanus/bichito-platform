a8a9ae80b7777960b749f736244696ff10af080b
Tamara <tylues@uc.cl>
2022-05-16 16:28:35 -0400

    fix(nav-bar):  edit style to use inline svg module

--- app/javascript/components/shared/nav-bar.vue
+++ app/javascript/components/shared/nav-bar.vue
@@ -6,8 +6,9 @@
     class="absolute inset-x-0 top-0 z-10 items-center py-5"
   >
     <img
+      v-svg-inline.sprite
+      class="mx-10 w-24 text-slate-800 md:mx-32 lg:mx-56 lg:w-32 "
       :src="require('images/bichito-logo.svg')"
-      class="mx-10 w-24 fill-blue-300 md:mx-32 lg:mx-56 lg:w-32"
       alt="Logo Bichito"
     >
   </nav>

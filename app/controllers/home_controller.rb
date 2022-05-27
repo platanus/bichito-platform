b3376d31fb3a35e20ceb2505a06e3b14b02d16f7
MariaJosefinaAlliende <moalliende@uc.cl>
2022-05-27 15:14:37 -0400

    chore(dotenv): use env variables directly in front

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
-  SHOW_CHALLENGES = ENV.fetch('SHOW_CHALLENGES') == 'true'
   def index
     @program_themes = ProgramTheme.all
     @members = Member.all
-    @show_challenges = SHOW_CHALLENGES
   end
 end

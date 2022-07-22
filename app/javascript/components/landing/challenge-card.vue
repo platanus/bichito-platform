43dd56be23c326dbe2ad4e0e806fa0d711900bb6
Tamara <tylues@uc.cl>
2022-07-22 14:57:17 -0400

    feat(challenge-card): add border to card

--- app/javascript/components/landing/challenge-card.vue
+++ app/javascript/components/landing/challenge-card.vue
@@ -46,7 +46,7 @@ const difficultyStyleVariant = computed(() => difficultyStyles[props.difficulty 
 </script>
 
 <template>
-  <div class="aspect-5/4 flex flex-col p-4 mx-2 space-y-4 min-w-[85%] bg-white rounded-3xl md:mx-0 md:w-full">
+  <div class="aspect-5/4 flex flex-col p-4 m-8 space-y-4 min-w-[85%] bg-white rounded-3xl border border-slate-200 shadow-xl md:mx-0 md:w-full">
     <div class="flex flex-row justify-between w-full ">
       <div class="text-sm font-light text-gray-500">
         {{ kindVariant }}
